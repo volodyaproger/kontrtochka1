376c8cf2c35c675ee12dcf46ec1948cd7141025a
volodyaproger <145913931+volodyaproger@users.noreply.github.com>
2023-10-29 22:04:46 +0300

    Update zanyatie3_n1.py

--- занятие 3/zanyatie3_n1.py
+++ занятие 3/zanyatie3_n1.py
@@ -1,4 +1,4 @@
-x = int(input())
-y = int(input())
-z = int(input())
+x = int(input('Введите число:'))
+y = int(input('Введите число:'))
+z = int(input('Введите число:'))
 print("Сумма:", x + y + z)
