c92004f3d69df802f441371ee49b6cb868a06482
volodyaproger <145913931+volodyaproger@users.noreply.github.com>
2023-10-29 22:05:39 +0300

    Update zanyatie3_n6.py

--- занятие 3/zanyatie3_n6.py
+++ занятие 3/zanyatie3_n6.py
@@ -1,7 +1,7 @@
-a1 = int(input())
-b1 = int(input())
-a2 = int(input())
-b2 = int(input())
+a1 = int(input('Введите число:'))
+b1 = int(input('Введите число:'))
+a2 = int(input('Введите число:'))
+b2 = int(input('Введите число:'))
 if (a1 + b1) % 2 == (a2 + b2) % 2:
     print("ДА")
 else:
