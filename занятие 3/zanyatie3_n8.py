63bb9d2e49e0075fd7bcfdf32fdec5cf9fbe73eb
volodyaproger <145913931+volodyaproger@users.noreply.github.com>
2023-10-29 22:06:04 +0300

    Update zanyatie3_n8.py

--- занятие 3/zanyatie3_n8.py
+++ занятие 3/zanyatie3_n8.py
@@ -1,6 +1,6 @@
-a = int(input())
-b = int(input())
-c = int(input())
+a = int(input('Введите число:'))
+b = int(input('Введите число:'))
+c = int(input('Введите число:'))
 if a == b == c:
     print("3")
 if (a == b and a != c) or (b == c and  b != a) or (a == c and b != c):
