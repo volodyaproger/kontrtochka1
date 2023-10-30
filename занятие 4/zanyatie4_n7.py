83fc6d8b010bcfd6c108b539f0263b144fd71736
volodyaproger <145913931+volodyaproger@users.noreply.github.com>
2023-10-30 09:01:09 +0300

    Update zanyatie4_n7.py

--- занятие 4/zanyatie4_n7.py
+++ занятие 4/zanyatie4_n7.py
@@ -1,5 +1,15 @@
+def F(n):
+    a = 1
+    for i in range(2, n + 1):
+        a *= i
+    return a
+
+def F1(n):
+    b = 0
+    for i in range(1, n + 1):
+        b += F(i)
+    return b
+
 n = int(input('Введите число:'))
-for a in range(1, n + 1):
-    for b in range(1, a + 1):
-        print(b, sep='', end='')
-    print()
+a = F1(n)
+print(a)
