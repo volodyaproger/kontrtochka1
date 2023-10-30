30dbe318cc8071916c35a674d754f3b942b33835
volodyaproger <145913931+volodyaproger@users.noreply.github.com>
2023-10-30 08:24:10 +0300

    Update zanyatie3_n6.py

--- занятие 3/zanyatie3_n6.py
+++ занятие 3/zanyatie3_n6.py
@@ -1,8 +1,22 @@
-a1 = int(input('Введите число:'))
-b1 = int(input('Введите число:'))
-a2 = int(input('Введите число:'))
-b2 = int(input('Введите число:'))
-if (a1 + b1) % 2 == (a2 + b2) % 2:
-    print("ДА")
-else:
-    print("НЕТ")
+def F(a1, a2, b1, b2):
+    c1 = 0
+    c2 = 0
+    if a1 or a2 %2==0:
+        c1 = 1
+    else:
+        c1 = 0
+    if b1 or b2 %2==0:
+        c2 = 1
+    else:
+        c2 = 0
+    if c1 == c2:
+        print('ДА')
+    else:
+        print('НЕТ')
+
+a1 = int(input())
+a2 = int(input())
+b1 = int(input())
+b2 = int(input())
+
+F(a1, a2, b1, b2)
