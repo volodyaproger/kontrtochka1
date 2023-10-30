4ab602e783db01e3b94bdc2ff73d7f4e06de11e4
volodyaproger <145913931+volodyaproger@users.noreply.github.com>
2023-10-30 08:54:14 +0300

    Update zanyatie4_n5.py

--- занятие 4/zanyatie4_n5.py
+++ занятие 4/zanyatie4_n5.py
@@ -1,5 +1,7 @@
-N = int(input('Введите число:'))
-summa = 0
-for num in range(1, N + 1):
-    summa += num ** 3
-print(summa)
+def F(n):
+    a = (n * (n + 1) // 2) ** 2
+    print(a)
+
+
+n = int(input('Введите число:'))
+F(n)
